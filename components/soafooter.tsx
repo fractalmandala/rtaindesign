850f5ad38ca328679110c6751ca3deb4dfd3ad58
amritbrhat <i.amrit.p@gmail.com>
2022-12-24 01:30:55 +0530

    2312willwulfkennamecorrection

--- components/soafooter.tsx
+++ components/soafooter.tsx
@@ -5,7 +5,7 @@ export default function Soafooter() {
   return (
     <div className={styles.soafooter}>
       <h4>A Project by <a href="https://brhat.in" target="_blank">Bṛhat</a></h4>
-      <p>All training kit content is taken from the extensively detailed <a href="https://github.com/willwulfken/MidJourney-Styles-and-Keywords-Reference" target="_blank">Styles and Keywords Reference by Willwulfken</a> at Github.</p>
+      <p>All training kit content is taken from the extensively detailed <a href="https://github.com/willwulfken/MidJourney-Styles-and-Keywords-Reference" target="_blank">Styles and Keywords Reference by Will Wulfken</a> at Github.</p>
     </div>
   )
 }
